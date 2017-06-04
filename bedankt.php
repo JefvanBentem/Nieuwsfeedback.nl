39a86dca393b4aff502e1441fc12e15a82498c1f
Evan <eje.handgraaf@gmail.com>
2017-06-05 00:04:18 +0200

    Mooiere bedank page

--- bedankt.php
+++ bedankt.php
@@ -4,8 +4,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bedankt!</title>
+    <style>
+    .containertekst {
+      width: 200px;
+      height: 200px;
+      background-color: #333333;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    </style>
   </head>
   <body>
-    <p>Bedankt voor de reactie!</p>
+    <div class="containertekst">
+      <p>Bedankt voor de reactie!</p>
+    </div>
   </body>
 </html>
