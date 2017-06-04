6e840da6b78c57b4d4549a25b185a264584fb767
Evan <eje.handgraaf@gmail.com>
2017-06-04 22:57:32 +0200

    Test

--- stemmen.php
+++ stemmen.php
@@ -1,5 +1,5 @@
 <?php
-if (!isset($_GET["nieuwsbriefid"]) or !isset($_GET["actie"])) {
+if (!isset($_GET['nieuwsbriefid']) or !isset($_GET['actie'])) {
   $error = "Er is iets fout gegaan!";
 } else {
   $actie = $_GET["actie"];
