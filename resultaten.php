49eff0372f625769d9569e84c3ae8e5a2146b117
Evan <eje.handgraaf@gmail.com>
2017-06-05 22:55:08 +0200

    Betere resultaat tabel

--- resultaten.php
+++ resultaten.php
@@ -30,51 +30,59 @@ if (!isset($_SESSION["ingelogd"])) {
     .selected {
       text-decoration: underline;
     }
+    .containertable {
+      overflow-x: auto;
+    }
+    tr:nth-child(even) {
+      background-color: #f2f2f2
+    }
     </style>
   </head>
   <body>
     <section>
-    <table>
-      <tr>
-        <th>Nieuwsbriefnaam</th>
-        <th>Likes</th>
-        <th>Dislikes</th>
-        <th>Aantal comments</th>
-        <th>Zie comments</th>
-        <th>Verwijder</th>
-      </tr>
-      <?php
-      //Zoek de hoeveelheid pagina's uit
-      $hoeveelheidqueries = $object->aantalnieuwsbrieven();
-      $hoeveelheidpaginas = $hoeveelheidqueries / 10;
-      $hoeveelheidpaginas = ceil($hoeveelheidpaginas);
-      //Huidige pagina nummer
-      $page = isset($_GET['page']) ? $_GET['page'] : 0;
-      //Vanaf wat wil je zien
-      $vanafwelknummerwiljezien = $page * 10;
-      //Krijg de nieuwsbrieven
-      $krijgallenieuwsbrieven = $object->krijgallenieuwsbrieven($vanafwelknummerwiljezien);
-      //Loop voor iedere nieuwsbrief
-      while ($array = mysqli_fetch_array($krijgallenieuwsbrieven)) {
-        //Zet de variabelen
-        $naam = htmlentities($array['randomid']);
-        $likes = $object->hoeveellikes($naam);
-        $dislikes = $object->hoeveeldislikes($naam);
-        $aantalcomments = $object->hoeveelcomments($naam);
-        $ziecomment = "<a href='ziecomments.php?nieuwsbrief=$naam'>&#916;</a>";
-        $verwijder = "<a href='verwijder.php?nieuwsbrief=$naam'>&#916;</a>";
-        //Display in tabel
-        echo "<tr>";
-        echo "<td>$naam</td>";
-        echo "<td>$likes</td>";
-        echo "<td>$dislikes</td>";
-        echo "<td>$aantalcomments</td>";
-        echo "<td>$ziecomment</td>";
-        echo "<td>$verwijder</td>";
-        echo "</tr>";
-      }
-      ?>
-    </table>
+    <div class="containertable">
+      <table>
+        <tr>
+          <th>Nieuwsbriefnaam</th>
+          <th>Likes</th>
+          <th>Dislikes</th>
+          <th>Aantal comments</th>
+          <th>Zie comments</th>
+          <th>Verwijder</th>
+        </tr>
+        <?php
+        //Zoek de hoeveelheid pagina's uit
+        $hoeveelheidqueries = $object->aantalnieuwsbrieven();
+        $hoeveelheidpaginas = $hoeveelheidqueries / 10;
+        $hoeveelheidpaginas = ceil($hoeveelheidpaginas);
+        //Huidige pagina nummer
+        $page = isset($_GET['page']) ? $_GET['page'] : 0;
+        //Vanaf wat wil je zien
+        $vanafwelknummerwiljezien = $page * 10;
+        //Krijg de nieuwsbrieven
+        $krijgallenieuwsbrieven = $object->krijgallenieuwsbrieven($vanafwelknummerwiljezien);
+        //Loop voor iedere nieuwsbrief
+        while ($array = mysqli_fetch_array($krijgallenieuwsbrieven)) {
+          //Zet de variabelen
+          $naam = htmlentities($array['randomid']);
+          $likes = $object->hoeveellikes($naam);
+          $dislikes = $object->hoeveeldislikes($naam);
+          $aantalcomments = $object->hoeveelcomments($naam);
+          $ziecomment = "<a href='ziecomments.php?nieuwsbrief=$naam'>&#916;</a>";
+          $verwijder = "<a href='verwijder.php?nieuwsbrief=$naam'>&#916;</a>";
+          //Display in tabel
+          echo "<tr>";
+          echo "<td>$naam</td>";
+          echo "<td>$likes</td>";
+          echo "<td>$dislikes</td>";
+          echo "<td>$aantalcomments</td>";
+          echo "<td>$ziecomment</td>";
+          echo "<td>$verwijder</td>";
+          echo "</tr>";
+        }
+        ?>
+      </table>
+    </div>
     <p>Pagina's:</p>
     <?php
     //Pagina systeem
