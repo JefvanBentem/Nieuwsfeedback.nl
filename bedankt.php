305b9e7885d643960a67c9c789492961c78fa9b1
Evan <eje.handgraaf@gmail.com>
2017-06-05 22:10:25 +0200

    Betere bedank page

--- bedankt.php
+++ bedankt.php
@@ -5,14 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="img/favi.png" sizes="16x16">
     <title>Bedankt!</title>
+    <link href="fonts/Franklin.css" rel="stylesheet">
     <style>
+    body {
+      font-family: 'Libre Franklin', sans-serif;
+    }
     .containertekst {
       width: 100vw;
       height: 90vh;
       display: flex;
       align-items: center;
       justify-content: center;
-      font-size: 150%;
       color: #2467a8;
     }
     * {
@@ -22,8 +25,10 @@
   </head>
   <body>
     <div class="containertekst">
-      <p>Bedankt voor uw reactie.</p>
-      <p>U wordt nu automatisch doorgestuurd naar de homepage.</p>
+      <div class="containertekst2">
+        <p style="font-size:1.2em;">Bedankt voor uw reactie.</p>
+        <p>U wordt nu automatisch doorgestuurd naar de homepage.</p>
+      </div>
     </div>
     <script>setTimeout(function(){window.location.href='http://hekmansuelmann.nl/'},5000);</script>
   </body>
