7952a1dbd9af8eaa3732249490d83eeaaa856447
Kaasmanneman <Kaasmanneman@users.noreply.github.com>
2017-06-16 21:23:58 +0200

    Responsive update

--- adminlogin.php
+++ adminlogin.php
@@ -61,6 +61,7 @@ if (isset($_POST['gebruikersnaam']) && isset($_POST['wachtwoord'])) {
     @media (max-width: 500px) {
       .forumcontainer {
         width: 90%;
+        padding-top: 50px;
       }
     }
     </style>
