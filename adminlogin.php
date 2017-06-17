5408804e753ffa7e91f840c186f0105a638a01b4
Evan <eje.handgraaf@gmail.com>
2017-06-17 17:46:16 +0200

    Kleinere padding top

--- adminlogin.php
+++ adminlogin.php
@@ -66,7 +66,7 @@ if (isset($_POST['gebruikersnaam']) && isset($_POST['wachtwoord'])) {
     @media (max-width: 500px) {
       .forumcontainer {
         width: 90%;
-        padding-top: 50px;
+        padding-top: 25px;
       }
     }
     </style>
